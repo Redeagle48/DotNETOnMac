086c012d3b86b463609e40857c6b5ac0a980e545
Redeagle48 <antoniopereira350@gmail.com>
2016-12-15 22:53:57 +0000

    Test .Net on Mac

---
+++ Patterns/Proxy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class Proxy
+    {
+        public Proxy() {}
+        public void execute() {
+            Console.WriteLine("teste"); 
+        }
+    }
+}
